7c484b2bf69b5989eb16c00b7cad8c88a462f9a1
Guilherme Rodovalho de Oliveira <guilherme.oliveira@invillia.com>
2021-03-12 09:32:07 -0300

    feat: esure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -1,8 +1,13 @@
 export enum HttpStatusCode {
+  ok = 200,
   noContent = 204,
-  unathorized = 401
+  badRequest = 400,
+  unathorized = 401,
+  notFaund = 404,
+  serverError = 500
 }
 
 export type HttpResponse = {
   statusCode: HttpStatusCode
+  body?: any
 }
