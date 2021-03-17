b1dd4fd48962baa2f6c1faeeb5ea45b6c9ddd081
Guilherme Rodovalho de Oliveira <guilherme.oliveira@invillia.com>
2021-03-16 23:45:06 -0300

    refactor: organize imports

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,7 +1,7 @@
 import { AxiosHttpClient } from './axios-http-client'
-import axios from 'axios'
-import faker from 'faker'
 import { HttpPostParams } from '@/data/protocols/http'
+import faker from 'faker'
+import axios from 'axios'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
