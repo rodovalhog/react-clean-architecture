a2b95a91f1ba4271ea7ddf8e86e8a4efc028c709
Guilherme <rodovalhogdeveloper@gmail.com>
2021-04-07 14:19:06 -0300

    feat: add component router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presetation/components'
+import '@/presetation/styles/global.scss'
 
 ReactDOM.render(
   <Router />,
