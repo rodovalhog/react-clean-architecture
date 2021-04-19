f71997e8300ca88a1efa2ee3e8d8a76d90f231bb
GUILHERME RODOVALHO DE OLIVEIRA <rodovalhogdeveloper@gmail.com>
2021-04-19 13:44:08 -0300

    test: ensure Login calls validations with correct email

--- src/presetation/pages/login/login.spec.tsx
+++ src/presetation/pages/login/login.spec.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import Login from './login'
+import faker from 'faker'
+import { Validation } from '@/presetation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidtionSpy
+}
+class ValidtionSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidtionSpy()
+  const sut = render(<Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Login Component', () => {
   test('Should start with initial state', () => {
+    afterEach(cleanup)
     const { sut } = makeSut()
 
     const errorWrap = sut.getByTestId('error-wrap')
@@ -31,9 +46,21 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('Campo obrigatorio')
     expect(passwordStatus.textContent).toBe('🏀')
   })
+
+  test('Should call validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    const emailFaker = faker.internet.email()
+    fireEvent.input(emailInput, { target: { value: emailFaker } })
+    expect(validationSpy.input).toEqual({
+      email: emailFaker
+    })
+  })
 })
 
 /*
+getByTestId = capitura o atributo data-testid das tag
 childElementCount = verifica se o componente foi chamado retornando um inteiro
-
+fireEvent= dispara o evento
+afterEach(cleanup) = limpa o compoenente para que ele não seja alterado pelos testes
 */
