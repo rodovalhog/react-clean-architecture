2e23b04c943708c0d385cde4098c5c03dd99fb3e
GUILHERME RODOVALHO DE OLIVEIRA <rodovalhogdeveloper@gmail.com>
2021-05-01 17:42:54 -0300

    refactor: move SUT create to a factoru method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,18 +1,25 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { MinLengthValidation } from './min-length-validation'
+import faker from 'faker'
 
-const MakeSut = (): MinLengthValidation => new MinLengthValidation('email', 5)
+const MakeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLengthValidation', () => {
   test('Should return erro if value is invalid', () => {
     const sut = MakeSut()
-    const error = sut.validate('123')
+    const error = sut.validate(faker.random.alphaNumeric(3))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
     const sut = MakeSut()
-    const error = sut.validate('12345')
+    const error = sut.validate(faker.random.alphaNumeric(5))
+    expect(error).toBeFalsy()
+  })
+
+  test('Should return falsy if value is empty', () => {
+    const sut = MakeSut()
+    const error = sut.validate(faker.random.alphaNumeric(0))
     expect(error).toBeFalsy()
   })
 })
