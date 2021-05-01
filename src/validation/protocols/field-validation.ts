492bcf7e2bedda42a960dba4e59d2d6904b007db
GUILHERME RODOVALHO DE OLIVEIRA <rodovalhogdeveloper@gmail.com>
2021-05-01 15:43:05 -0300

    feat: ensure MinLengtuValidation return error if value invalid

--- src/validation/protocols/field-validation.ts
+++ src/validation/protocols/field-validation.ts
@@ -1,4 +1,5 @@
 export interface FieldValidation {
   field: string
   validate (value: string): Error
+  test? (value: string): string
 }
---
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -0,0 +1,9 @@
+import { InvalidFieldError } from '../../errors'
+import { FieldValidation } from '@/validation/protocols'
+
+export class MinLengthValidation implements FieldValidation {
+  constructor (readonly field: string, private readonly minLength: number) {}
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
